6360a4cf8c5128f23ebb696464816c959c2cdb23
Guicho <luiaballz.laor@gmail.com>
2023-07-01 13:18:58 -0600

    Se añade nuevo proyecto Challenger y modificaciones a las clases de los cursos

--- Java/ByteBank_Heredado/src/ControlBonificaciones.java
+++ Java/ByteBank_Heredado/src/ControlBonificaciones.java
@@ -3,14 +3,15 @@ public class ControlBonificaciones {
 
     public double registrarSalario(Funcionario objetoFuncionario){
         this.sumatoria = objetoFuncionario.obtenerBonificación() + this.sumatoria;
+        System.out.println("Calculo actual: " + sumatoria);
         return sumatoria;
     }
 
-    /*public double registrarSalario(Gerente objetoGerente){
+    public double registrarSalario(Gerente objetoGerente){
         this.sumatoria = objetoGerente.obtenerBonificacion() + this.sumatoria;
         return sumatoria;
     }
-
+    /*
     public double registrarSalario(Contador objetoContador){
         this.sumatoria = objetoContador.obtenerBonificación() + this.sumatoria;
         return sumatoria;
--- Java/ByteBank_Heredado/src/TesteControlBonificaciones.java
+++ Java/ByteBank_Heredado/src/TesteControlBonificaciones.java
@@ -2,19 +2,19 @@ public class TesteControlBonificaciones {
     public static void main(String[] args) {
         Funcionario funcionarioIliany = new Funcionario();
         funcionarioIliany.ingresarNombre("Iliany");
-        funcionarioIliany.ingresarSalario(9000.00);
+        funcionarioIliany.ingresarSalario(2000.00);
         //Se crea objeto de control de bonificaciones
         ControlBonificaciones controlBonificaciones = new ControlBonificaciones();
         System.out.println("Bonificación funcionario " + funcionarioIliany.obtenerNombre() + ": " + controlBonificaciones.registrarSalario(funcionarioIliany));
-        //Ahora viene un ejemplo de polimorfismo
+       //Ahora viene un ejemplo de polimorfismo
         Gerente gerenteMeme = new Gerente();
         gerenteMeme.ingresarNombre("Meme");
         gerenteMeme.ingresarSalario(10000.00);
         System.out.println("Bonificación gerente " + gerenteMeme.obtenerNombre() + ": " + controlBonificaciones.registrarSalario(gerenteMeme));
-        //Se crea un objeto contador
+        /*//Se crea un objeto contador
         Contador contadorWilliam = new Contador();
         contadorWilliam.ingresarNombre("William");
         contadorWilliam.ingresarSalario(5000.00);
-        System.out.println("Bonificación contador " + contadorWilliam.obtenerNombre() + ": " + controlBonificaciones.registrarSalario(contadorWilliam));
+        System.out.println("Bonificación contador " + contadorWilliam.obtenerNombre() + ": " + controlBonificaciones.registrarSalario(contadorWilliam));*/
     }
 }

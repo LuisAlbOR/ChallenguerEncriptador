e30f390ff69cd0ca2b6ada85586b463edd71107f
Guicho <luiaballz.laor@gmail.com>
2023-07-28 19:18:29 -0600

    Se modifica la clase agregando mas detalles sobre arrayList

--- Java/ByteBank/bytebank/MX/Com/Bytebank/Test/TestArrayList.java
+++ Java/ByteBank/bytebank/MX/Com/Bytebank/Test/TestArrayList.java
@@ -39,14 +39,26 @@ public class TestArrayList {
         /*
         Lista con capacidad predefinida.
 
-Decíamos que el ArrayList es un array dinámico, es decir, debajo de la tela se usa un array,
-pero sin preocuparse por los detalles y limitaciones.
-Ahora piense que necesita crear una lista que represente a los 26 estados de Brasil.
-Le gustaría usar un ArrayList para "escapar" del array,
-pero sabe que ArrayList crea un array automáticamente, del tamaño que la clase considere conveniente.
+        Decíamos que el ArrayList es un array dinámico, es decir, debajo de la tela se usa un array,
+        pero sin preocuparse por los detalles y limitaciones.
+        Ahora piense que necesita crear una lista que represente a los 26 estados de Brasil.
+        Le gustaría usar un ArrayList para "escapar" del array,
+        pero sabe que ArrayList crea un array automáticamente, del tamaño que la clase considere conveniente.
 
-¿No hay alguna forma de crear esta lista definiendo el tamaño del array? Por supuesto que lo es y es muy sencillo.
- El constructor de la clase ArrayList es sobrecargado y tiene un parámetro que recibe la capacidad:
+        ¿No hay alguna forma de crear esta lista definiendo el tamaño del array? Por supuesto que lo es y es muy sencillo.
+         El constructor de la clase ArrayList es sobrecargado y tiene un parámetro que recibe la capacidad:
          */
+        ArrayList lista = new ArrayList(26); //capacidad inicial
+        //La lista sigue siendo dinámica, ¡pero el tamaño de la matriz inicial es 26!
+        /*
+        Lista a partir de otra
+
+        Otra forma de inicializar una lista es basada en otra que es muy común en el día a día. Para esto,
+        ArrayList tiene un constructor más que recibe la lista base:
+         */
+        lista.add("RJ");
+        lista.add("SP");
+        //otros estados
+        ArrayList nueva = new ArrayList(lista); //creando basada en la primera lista
     }
 }
